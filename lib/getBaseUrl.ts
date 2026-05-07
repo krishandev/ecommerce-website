4e46e15a86a84759e7505f606f61030028c48458
Krishan Dev <krishandev.0102@gmail.com>
2026-05-07 16:01:54 +0530

    added public site url in env local

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,7 +1,3 @@
 export function getBaseUrl() {
-  if (process.env.NODE_ENV === "production") {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  return "http://localhost:3000";
+  return process.env.NEXT_PUBLIC_SITE_URL!;
 }
